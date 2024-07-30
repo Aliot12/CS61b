93c21749e9b4e45b85296d13c384d53deef554dc
“Aliot12” <1430509622@qq.com>
2024-07-30 21:51:10 +0800

    finish

--- proj2a/src/main/Main.java
+++ proj2a/src/main/Main.java
@@ -17,9 +17,8 @@ public class Main {
         NgordnetServer hns = new NgordnetServer();
         NGramMap ngm = new NGramMap(TOP_14337_WORDS_FILE,TOTAL_COUNTS_FILE);
         //NGramMap ngm = new NGramMap(SHORT_WORDS_FILE, TOTAL_COUNTS_FILE);
-
         hns.startUp();
-        hns.register("history", new DummyHistoryHandler());
+        hns.register("history", new HistoryHandler(ngm));
         hns.register("historytext", new HistoryTextHandler(ngm));
 
         System.out.println("Finished server startup! Visit http://localhost:4567/ngordnet_2a.html");
